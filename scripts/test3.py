54530808d9b1fd0de3733e0a5e13d282b7def741
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2019-11-01 11:22:12 +0900

    Update test3.py

--- scripts/test3.py
+++ scripts/test3.py
@@ -5,11 +5,10 @@ from geometry_msgs.msg import Twist
 
 rospy.init_node("forward") #Give the node a name
 
-
 pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
 while not rospy.is_shutdown():
     vel  = Twist() #initialize
-    vel  = 0.2
+    vel.linear.x  = 0.2
     print vel
     pub.publish(vel)
