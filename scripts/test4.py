ea031a9a89bab8c1f9fb3d6b7a9ead07d4612f12
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2019-11-01 15:15:15 +0900

    Create test4.py

---
+++ scripts/test4.py
@@ -0,0 +1,17 @@
+#Keep going forward and stop when bumper activated
+
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from ca_msgs.msg import Bumper
+
+rospy.init_node("SBC") #Give the node a name
+
+pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+bumper = rospy.Subscriber("bumper", Bumper, queue_size=10)
+
+while not rospy.is_shutdown():
+    vel  = Twist() #initialize
+    vel.linear.x  = 0.1
+    print vel
+    pub.publish(vel)
