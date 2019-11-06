1ac8e811f332794801e7dbfc51c0fea7a041769d
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2019-11-06 19:15:27 +0900

    Update test4.py

--- scripts/test4.py
+++ scripts/test4.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
-
+#Keep going forward and stop when bumper activated
 import rospy
 from geometry_msgs.msg import Twist
 from ca_msgs.msg import Bumper
 
-rospy.init_node("SBC") #Give the node a name
+rospy.init_node("GoAndStop") #Give the node a name
 
 pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-bumper = rospy.Subscriber("bumper", Bumper, queue_size=10)
 
-#Keep going forward and stop when bumper activated
-while not rospy.is_shutdown():
-    vel  = Twist() #initialize
-    vel.linear.x  = 0.1
-    print vel
-    pub.publish(vel)
+def maim():
+    sub = rospy.Subscriber("bumper", Bumper, callback)
+    while not rospy.is_shutdown():
+        vel  = Twist() #initialize
+        vel.linear.x  = 0.1
+        print vel
+        pub.publish(vel)
+
+def callback(bumper):
+    print bumper
+    stop_vel = Twist()
+    stop_vel.linear.x = 0
+    rate = rospy.Rate(10.0)
+    for i in range(5):
+        pub.publish(stop_vel)
+        rate.sleep()
+
+if __name__ == "__main__":
+    main()
