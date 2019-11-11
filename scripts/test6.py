3134d06f0d88a323c9bf444aaa98cb3bf67ca2f1
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2019-11-11 15:20:23 +0900

    Update test6.py

--- scripts/test6.py
+++ scripts/test6.py
@@ -53,7 +53,7 @@ class LCS():
                 ave_c = (light_cl + light_cr)/2
                 ave_r = (light_l + light_fl)/2
                 ave_l = (light_r + light_fr)/2
-                THRESHOLD = 100
+                THRESHOLD = 10
                 if ave_c>THRESHOLD or ave_r>THRESHOLD or ave_l>THRESHOLD: #detect obstacle
                     if (ave_l < ave_c) and (ave_r < ave_c): #obstacle is in front
                         if ave_l > ave_r: #direction of right is more safety
