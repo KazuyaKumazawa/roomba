e422779d0b280ca3ef916de0d665dbcfbf8a34a2
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2020-01-24 21:23:36 +0900

    Update 2nd_layer_record.py

--- scripts/2nd_layer_record.py
+++ scripts/2nd_layer_record.py
@@ -30,16 +30,6 @@ class LCS():
     def run(self):
         rate = rospy.Rate(10)
         vel  = Twist()
-        light_l = self.bumper.light_signal_left
-        light_fl = self.bumper.light_signal_front_left
-        light_cl = self.bumper.light_signal_center_left
-        light_cr = self.bumper.light_signal_center_right
-        light_fr = self.bumper.light_signal_front_right
-        light_r = self.bumper.light_signal_right
-        ave_c = (light_cl + light_cr)/2
-        ave_l = (light_l + light_fl)/2
-        ave_r = (light_r + light_fr)/2
-        THRESHOLD = 10
         while not rospy.is_shutdown():
             with open('record.csv', 'a') as r:
                 writer = csv.writer(r)
@@ -68,6 +58,16 @@ class LCS():
                     rate.sleep()
             else:
                 #2nd layer
+                light_l = self.bumper.light_signal_left
+                light_fl = self.bumper.light_signal_front_left
+                light_cl = self.bumper.light_signal_center_left
+                light_cr = self.bumper.light_signal_center_right
+                light_fr = self.bumper.light_signal_front_right
+                light_r = self.bumper.light_signal_right
+                ave_c = (light_cl + light_cr)/2
+                ave_l = (light_l + light_fl)/2
+                ave_r = (light_r + light_fr)/2
+                THRESHOLD = 10
                 if ave_c>THRESHOLD or ave_r>THRESHOLD or ave_l>THRESHOLD: #detect obstacle
                     if (ave_l < ave_c) and (ave_r < ave_c): #obstacle is in front
                         if ave_l > ave_r: #direction of right is more safety
