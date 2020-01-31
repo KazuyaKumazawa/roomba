8ad2541118456b20d547b8766f510ade41f1645e
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2020-01-31 14:03:51 +0900

    Create test_ir.py

---
+++ scripts/test_ir.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import rospy
+import csv
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
+from ca_msgs.msg import Bumper
+from std_msgs.msg import Ulnt16
+
+class LCS():
+    def __init__(self):
+        self.cmd_vel = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.dock = rospy.Publisher("dock", Empty, queue_size=10)
+        self.bumper = Bumper()
+        rospy.Subscriber("bumper", Bumper, self.callback)
+        self.ir_omni = Ulnt16()
+        rospy.Subscriber("ir_omni", Ulnt16, self.callback)
+            
+    def callback(self,messages):
+        self.bumper = messages
+        self.ir_omni = messages
+        
+    def run(self):
+        rate = rospy.Rate(10)
+        vel  = Twist()
+        light_l = self.bumper.light_signal_left
+        light_fl = self.bumper.light_signal_front_left
+        light_cl = self.bumper.light_signal_center_left
+        light_cr = self.bumper.light_signal_center_right
+        light_fr = self.bumper.light_signal_front_right
+        light_r = self.bumper.light_signal_right
+        ir = self.ir_omni
+        while not rospy.is_shutdown():
+            #1st layer
+            for i in range(128): #12.8sec*0.5rad/sec~2Pi
+                vel.linear.x  = 0
+                vel.angular.z = 0.5
+                print vel
+                with open('ir.csv', 'a') as r:
+                    writer = csv.writer(r)
+                    writer.writerow([vel.linear.x, vel.angular.z, ir])
+                self.cmd_vel.publish(vel)
+                rate.sleep()
+                    
+            
+
+if __name__ == '__main__':
+    rospy.init_node("LCS")
+    LCS().run()
