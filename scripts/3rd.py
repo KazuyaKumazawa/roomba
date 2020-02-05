e904e846c406cda7f355218645b360f5a68e0c1f
KazuyaKumazawa <52956710+KazuyaKumazawa@users.noreply.github.com>
2020-02-06 00:03:15 +0900

    Update 3rd.py

--- scripts/3rd.py
+++ scripts/3rd.py
@@ -91,7 +91,7 @@ class LCS():
                     writer = csv.writer(r)
                     writer.writerow([vel.linear.x, vel.angular.z, self.bumper.is_left_pressed, self.bumper.is_right_pressed, light_l, light_fl, light_cl, light_cr, light_fr, light_r, ir])
                 rate.sleep()
-            '''
+                '''
             elif (ir == 165): #Green Buoy and Force Field
                 vel.linear.x  = 0.01
                 vel.angular.z = -1
@@ -110,7 +110,7 @@ class LCS():
                     writer = csv.writer(r)
                     writer.writerow([vel.linear.x, vel.angular.z, self.bumper.is_left_pressed, self.bumper.is_right_pressed, light_l, light_fl, light_cl, light_cr, light_fr, light_r, ir])
                 rate.sleep()
-            '''
+                '''
             elif (ir == 173) or (ir == 161) or (ir == 165) or (ir == 169): #near dock
                 vel.linear.x  = 0
                 vel.angular.z = 0
